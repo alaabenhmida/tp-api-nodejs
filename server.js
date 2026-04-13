9e128440f681a48070dad18b8b48a67d87c2cecc
alaabenhmida <ben.hmida.ala1@gmail.com>
2026-04-13 19:28:19 +0100

    test: erreur intentionnelle pour tester le CD

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./config/database');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-
+throw new Error('Erreur intentionnelle pour tester le pipeline');
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
 });
